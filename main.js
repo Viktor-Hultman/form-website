9d25d911b5b4b7ce59e0ae175db33f63130e9db6
Viktor Hultman <viktor-hultman@hotmail.com>
2020-09-29 09:16:10 +0200

    Adding js validation for sign-up form as well as using local storage to save the name of the user which displays later in a thank you message

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
-// This code snippet below is made from a videotutorial done by Netninja: https://www.youtube.com/watch?v=b0pxAb_yy2U&ab_channel=TheNetNinja.
 
-// Creation of variables and connect them to the form of the sign up page as well ass the error message before the submit button
-let form  = document.forms.signUpForm;
-let error = document.getElementById('error');
+document.addEventListener('invalid', (function(){
+    return function(e){
+        //prevent the browser from showing default error bubble/ hint
+        e.preventDefault();
+    };
+})(), true);
 
-// A function which checks if any form-field is left empty when the submit button is clicked
-form.onsubmit = function(){
-    if(form.name.value == "" || form.email.value == "" || form.phone.value == "" || form.address.value == "" || form.zip.value == "" || form.package.value == ""){
-        error.innerHTML = "You need to fill out the entire form to register!";
-        return false;
-    } else{
-        error.innerHTML = "";
-        return true;
+//Validation function which fires of when the register button is clicked
+function giveInfo() {
+
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let address = document.getElementById("address");
+    let zip = document.getElementById("zip");
+    let package = document.getElementById("package");
+
+    //Creates a variable and set the value = to what is put in the name input field
+    let nameValue = document.getElementById("name").value;
+
+    //Then stores the variable value in the local storage to be used in the thank you message
+    localStorage.setItem("textvalue", nameValue);
+
+    //Checks if the current input in the fields are not valid, then adds some text to the message div if they are 
+    if (!name.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please enter your name!";
+    } else if (!email.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please enter your email address!";
+    } else if (!phone.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please enter your phone number!";
+    } else if (!address.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please enter your address!";
+    } else if (!zip.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please enter your zip code!";
+    } else if (!package.checkValidity()) {
+        document.getElementById("error").innerHTML = "Please select one of the package options!";
+    } else {
+        document.getElementById("error").innerHTML = " ";
     }
-};
+}
+
+
